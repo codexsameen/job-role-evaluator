a6a8534c3407c3395fc2408876eb7643d876ec74
Sameen Islam <sameen11@hotmail.com>
2026-03-10 21:18:28 +0000

    adding proxy for azure functions

--- serve.py
+++ serve.py
@@ -1,5 +1,11 @@
+import os
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 from flask import Flask, request, Response
 import requests
+
+AUTH_COOKIE = os.environ.get('AUTH_COOKIE', '')
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -7,15 +13,38 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/api/<path:path>', methods=['GET','POST','DELETE','PUT','PATCH'])
-def proxy(path):
-    url = f'http://localhost:7071/api/{path}'
+@app.route('/.auth/<path:path>', methods=['GET','POST'])
+def auth_proxy(path):
+    url = f'https://job-eval.tools.sameen.dev/.auth/{path}'
     resp = requests.request(
         method=request.method,
         url=url,
         headers={k: v for k, v in request.headers if k != 'Host'},
         data=request.get_data(),
         params=request.args,
+        cookies=request.cookies,
+        verify=False,
+        allow_redirects=False,
+    )
+    return Response(
+        resp.content,
+        status=resp.status_code,
+        mimetype=resp.headers.get('Content-Type', 'application/json'),
+    )
+
+@app.route('/api/<path:path>', methods=['GET','POST','DELETE','PUT','PATCH'])
+def proxy(path):
+    url = f'https://job-eval.tools.sameen.dev/api/{path}'
+    headers = {k: v for k, v in request.headers if k != 'Host'}
+    headers['Cookie'] = AUTH_COOKIE
+    resp = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=request.get_data(),
+        params=request.args,
+        verify=False,
+        allow_redirects=False,
     )
     return Response(
         resp.content,
